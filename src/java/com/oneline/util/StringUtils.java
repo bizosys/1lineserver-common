003ba16f790be2e14e47325474ef4e654ea8ed24
Abinasha Karana <abinash@bizosys.com>
2013-02-27 20:08:52 +0530

    Fast Split with Object Reuse
    This is 10 times faster than the JDK String tokenizer implementation.
    It will be very helpful for map-reduce work.

--- src/java/com/oneline/util/StringUtils.java
+++ src/java/com/oneline/util/StringUtils.java
@@ -179,6 +179,52 @@ public class StringUtils {
 		  return result;
 	  }	  
 	  
+	  public static void fastSplit( final String[] result, final int[] positions, 
+		final String text, final char separator) {
+		  
+		if (text == null) return;
+		if (text.length() == 0) return;
+
+		int index1 = 0;
+		int index2 = text.indexOf(separator);
+
+		int pos = -1;
+		int resultSeq = 0;
+		if ( index2 >= 0 ) {
+			String token = null;
+			while (index2 >= 0) {
+				pos++;
+				for ( int aPos: positions ) {
+					if ( pos != aPos) continue;
+					token = text.substring(index1, index2);
+					result[resultSeq++] = token;
+					break;
+				}
+				index1 = index2 + 1;
+				index2 = text.indexOf(separator, index1);
+				if ( index2 < 0 ) index1--;
+			}
+		            
+			if (index1 < text.length() - 1) {
+				pos++;
+				for ( int aPos: positions ) {
+					if ( pos != aPos) continue;
+					result[resultSeq++] = text.substring(index1+1);
+					break;
+				}
+			}
+			  
+		  } else {
+			  pos++;
+			  for ( int aPos: positions ) {
+				  if ( pos != aPos) continue;
+					result[resultSeq++] = text;
+				  break;
+			  }
+		  }
+	  }	  
+	  
+	  
 	  public static String firstTokens(final String text, char separator, int tokens) {
 		  if (isEmpty(text)) return null;
 
